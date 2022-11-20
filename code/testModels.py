60332bef123c4ab1a73ee1d9bf82c7c9483f8079
Gabriel El Haddad <35818559+Gabriel-El-Haddad@users.noreply.github.com>
2022-11-20 00:31:36 -0500

    test models

--- code/testModels.py
+++ code/testModels.py
@@ -85,7 +85,6 @@ def train(model,
 
 
 if __name__ == "__main__":
-    start = time.time()
     transform = A.Compose(
         [
             A.Resize(height=90, width=90),
@@ -104,10 +103,29 @@ if __name__ == "__main__":
     #load dataset
     training = Segmentation_DataLoader("./data/ISIC2018_Task1-2_Training_Input", "./data/ISIC2018_Task1_Training_GroundTruth", transformation=transform)
 
+    train_loader = torch.utils.data.DataLoader(training, batch_size=1, shuffle=False)
+
+    #load trained algorithm
     model = model()
-    train(model, training, batch_size=100, learning_rate=0.001, weight_decay=0.0, num_iter=100, plot=True)
+    model.load_state_dict(torch.load("./code/saved_models/seg_model100x100.pth"))
 
-    torch.save(model.state_dict(), "./code/saved_models/seg_model100x100.pth")
+    for imgs, labels in iter(train_loader):
+        fig, axarr = plt.subplots(nrows=1,ncols=3)
+        plt.sca(axarr[0]) 
+        plt.imshow(imgs.reshape(3, 90, 90).permute(1, 2, 0))
+        plt.title("Image")
+        plt.sca(axarr[1])
+        plt.imshow((model(imgs).reshape(1, 90, 90).permute(1, 2, 0)).detach().numpy())
+        plt.title("Prediction")
+        plt.sca(axarr[2])
+        plt.imshow(labels.permute(1, 2, 0))
+        plt.title("Ground Truth")
+        plt.show()
 
-    end = time.time()
-    print(str((end - start)//60) + "MINS")     
+    # model = model()
+    # train(model, training, batch_size=100, learning_rate=0.001, weight_decay=0.0, num_iter=100, plot=True)
+
+    # torch.save(model.state_dict(), "./code/saved_models/seg_model100x100.pth")
+
+    # end = time.time()
+    # print(str((end - start)//60) + "MINS")     
